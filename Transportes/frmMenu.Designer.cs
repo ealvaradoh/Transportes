a5944ca1e261c80b5f70787b514be31b41284c53
ealvaradoh <ernesto_roney@outlook.com>
2020-01-02 20:59:44 -0600

    Cambios menores: Se estructura sistema para tener separado el función del sistema y la parte visual.

--- Transportes/frmMenu.Designer.cs
+++ Transportes/frmMenu.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(845, 463);
+            this.ClientSize = new System.Drawing.Size(1067, 515);
             this.IsMdiContainer = true;
             this.Name = "frmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
