8580505bf20524b5edd62109bf6feb227418bebc
ealvaradoh <ernesto_roney@outlook.com>
2020-01-03 08:42:12 -0600

    Se agregan botones al menú, se inicia ventana login.

---
+++ Transportes/frmLogin.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Transportes
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(34, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Conexión a base de datsos";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Location = new System.Drawing.Point(34, 133);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos de usuario";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(167, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(167, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(19, 61);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(167, 22);
+            this.textBox4.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(34, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(34, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancelar";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 347);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmLogin";
+            this.Text = "frmLogin";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
--- Transportes/frmMenu.Designer.cs
+++ Transportes/frmMenu.Designer.cs
@@ -28,23 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.boletosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.encomiendasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturaciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controlDeUsuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeBoletosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeEncomiendasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturarServiciosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaDeFacturasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuariosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loginToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.catálogosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.busesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.conductoresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.personalABordoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lugaresSalidaDestinoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.boleteríaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.boletosToolStripMenuItem,
+            this.encomiendasToolStripMenuItem,
+            this.facturaciónToolStripMenuItem,
+            this.controlDeUsuariosToolStripMenuItem,
+            this.catálogosToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 33);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // boletosToolStripMenuItem
+            // 
+            this.boletosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaDeBoletosToolStripMenuItem});
+            this.boletosToolStripMenuItem.Name = "boletosToolStripMenuItem";
+            this.boletosToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.boletosToolStripMenuItem.Text = "Boletos";
+            // 
+            // encomiendasToolStripMenuItem
+            // 
+            this.encomiendasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaDeEncomiendasToolStripMenuItem});
+            this.encomiendasToolStripMenuItem.Name = "encomiendasToolStripMenuItem";
+            this.encomiendasToolStripMenuItem.Size = new System.Drawing.Size(109, 24);
+            this.encomiendasToolStripMenuItem.Text = "Encomiendas";
+            // 
+            // facturaciónToolStripMenuItem
+            // 
+            this.facturaciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facturarServiciosToolStripMenuItem,
+            this.listaDeFacturasToolStripMenuItem});
+            this.facturaciónToolStripMenuItem.Name = "facturaciónToolStripMenuItem";
+            this.facturaciónToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.facturaciónToolStripMenuItem.Text = "Facturación";
+            // 
+            // controlDeUsuariosToolStripMenuItem
+            // 
+            this.controlDeUsuariosToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.controlDeUsuariosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.usuariosToolStripMenuItem,
+            this.loginToolStripMenuItem});
+            this.controlDeUsuariosToolStripMenuItem.Name = "controlDeUsuariosToolStripMenuItem";
+            this.controlDeUsuariosToolStripMenuItem.Size = new System.Drawing.Size(149, 24);
+            this.controlDeUsuariosToolStripMenuItem.Text = "Control de usuarios";
+            // 
+            // listaDeBoletosToolStripMenuItem
+            // 
+            this.listaDeBoletosToolStripMenuItem.Name = "listaDeBoletosToolStripMenuItem";
+            this.listaDeBoletosToolStripMenuItem.Size = new System.Drawing.Size(189, 26);
+            this.listaDeBoletosToolStripMenuItem.Text = "Lista de boletos";
+            // 
+            // listaDeEncomiendasToolStripMenuItem
+            // 
+            this.listaDeEncomiendasToolStripMenuItem.Name = "listaDeEncomiendasToolStripMenuItem";
+            this.listaDeEncomiendasToolStripMenuItem.Size = new System.Drawing.Size(227, 26);
+            this.listaDeEncomiendasToolStripMenuItem.Text = "Lista de encomiendas";
+            // 
+            // facturarServiciosToolStripMenuItem
+            // 
+            this.facturarServiciosToolStripMenuItem.Name = "facturarServiciosToolStripMenuItem";
+            this.facturarServiciosToolStripMenuItem.Size = new System.Drawing.Size(196, 26);
+            this.facturarServiciosToolStripMenuItem.Text = "Facturar servicios";
+            // 
+            // listaDeFacturasToolStripMenuItem
+            // 
+            this.listaDeFacturasToolStripMenuItem.Name = "listaDeFacturasToolStripMenuItem";
+            this.listaDeFacturasToolStripMenuItem.Size = new System.Drawing.Size(196, 26);
+            this.listaDeFacturasToolStripMenuItem.Text = "Lista de facturas";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            this.usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            this.usuariosToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.usuariosToolStripMenuItem.Text = "Usuarios";
+            // 
+            // loginToolStripMenuItem
+            // 
+            this.loginToolStripMenuItem.Name = "loginToolStripMenuItem";
+            this.loginToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.loginToolStripMenuItem.Text = "Login";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1067, 29);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Top | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.toolStripStatusLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(200, 24);
+            this.toolStripStatusLabel1.Text = "Servidor:";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.AutoSize = false;
+            this.toolStripStatusLabel2.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((System.Windows.Forms.ToolStripStatusLabelBorderSides.Top | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right)));
+            this.toolStripStatusLabel2.Enabled = false;
+            this.toolStripStatusLabel2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(200, 24);
+            this.toolStripStatusLabel2.Text = "Usuario:";
+            this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // catálogosToolStripMenuItem
+            // 
+            this.catálogosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.busesToolStripMenuItem,
+            this.conductoresToolStripMenuItem,
+            this.personalABordoToolStripMenuItem,
+            this.clientesToolStripMenuItem,
+            this.lugaresSalidaDestinoToolStripMenuItem,
+            this.boleteríaToolStripMenuItem});
+            this.catálogosToolStripMenuItem.Name = "catálogosToolStripMenuItem";
+            this.catálogosToolStripMenuItem.Size = new System.Drawing.Size(108, 29);
+            this.catálogosToolStripMenuItem.Text = "Catálogos";
+            // 
+            // busesToolStripMenuItem
+            // 
+            this.busesToolStripMenuItem.Name = "busesToolStripMenuItem";
+            this.busesToolStripMenuItem.Size = new System.Drawing.Size(283, 30);
+            this.busesToolStripMenuItem.Text = "Buses";
+            // 
+            // conductoresToolStripMenuItem
+            // 
+            this.conductoresToolStripMenuItem.Name = "conductoresToolStripMenuItem";
+            this.conductoresToolStripMenuItem.Size = new System.Drawing.Size(283, 30);
+            this.conductoresToolStripMenuItem.Text = "Conductores";
+            // 
+            // personalABordoToolStripMenuItem
+            // 
+            this.personalABordoToolStripMenuItem.Name = "personalABordoToolStripMenuItem";
+            this.personalABordoToolStripMenuItem.Size = new System.Drawing.Size(283, 30);
+            this.personalABordoToolStripMenuItem.Text = "Personal a bordo";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(283, 30);
+            this.clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // lugaresSalidaDestinoToolStripMenuItem
+            // 
+            this.lugaresSalidaDestinoToolStripMenuItem.Name = "lugaresSalidaDestinoToolStripMenuItem";
+            this.lugaresSalidaDestinoToolStripMenuItem.Size = new System.Drawing.Size(283, 30);
+            this.lugaresSalidaDestinoToolStripMenuItem.Text = "Lugares Salida/Destino";
+            // 
+            // boleteríaToolStripMenuItem
+            // 
+            this.boleteríaToolStripMenuItem.Name = "boleteríaToolStripMenuItem";
+            this.boleteríaToolStripMenuItem.Size = new System.Drawing.Size(283, 30);
+            this.boleteríaToolStripMenuItem.Text = "Boletería";
             // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 515);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
             this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menú";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem boletosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeBoletosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem encomiendasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeEncomiendasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facturaciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facturarServiciosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaDeFacturasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controlDeUsuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuariosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loginToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ToolStripMenuItem catálogosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem busesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem conductoresToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem personalABordoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lugaresSalidaDestinoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem boleteríaToolStripMenuItem;
     }
 }
 
